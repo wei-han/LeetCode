474013d8b6b3a0e0178979438f06c6441916c3f9
Han Wei <jensonwei2012@gmail.com>
2017-07-21 16:19:22 -0400

    Update and rename 046_Permutations to 046_Permutations.cpp

--- 046_Permutations.cpp
+++ 046_Permutations.cpp
@@ -25,3 +25,32 @@ public:
         
     }
 };
+
+//no reference and no swap back method is the same !!
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+            
+        vector<vector<int>> ans;
+        per(ans, nums, 0);
+        return ans;
+       
+        
+    }
+    void per(vector<vector<int>> &ans, vector<int> nums, int begin)
+    {
+        if (begin >= nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        
+        for (int i = begin; i < nums.size(); i++)
+        {
+            swap(nums[begin], nums[i]);
+            per(ans, nums, begin+1);
+           // swap(nums[begin], nums[i]);
+        }
+        
+    }
+};
