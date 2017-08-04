2521dbd74cffc4b243fa6a2cbf170245a8bc4c7d
Han Wei <jensonwei2012@gmail.com>
2017-08-04 14:18:15 -0400

    Create 093_Restore IP Addresses.cpp

---
+++ 093_Restore IP Addresses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        
+    }
+    
+    void solve(string &s, string item, vector<string> ans)
+    {
+        if(item.size() == s.size()+3)
+        {
+            ans.push_back(item);
+            return;
+        }
+        
+        for(int i = begin; i < s.length(); i++)
+        {
+            if(int temp = stoi(s.c_str(), , ))
+        }
+    }
+};
