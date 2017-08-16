71787646c6527d4d0f902f0df4787da437aefc73
Han Wei <jensonwei2012@gmail.com>
2017-08-15 23:46:32 -0400

    Update 143_Reorder List.cpp

--- 143_Reorder List.cpp
+++ 143_Reorder List.cpp
@@ -36,3 +36,47 @@ public:
         
     }
 }
+
+
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if(!head || !head->next) return;
+        
+        //find the middle one
+        ListNode *p1, *p2;
+        p1 = p2 = head;
+        while(p2->next && p2->next->next)
+        {
+            p1 = p1->next;
+            p2 = p2->next->next;
+        }
+        
+        //cut the list to two
+        ListNode* head2 = p1->next;
+        p1->next = NULL;
+        
+        //reverse the second list
+        ListNode* temp = head2->next;
+        head2->next = NULL;
+        while(temp)
+        {
+            p1 = temp->next;
+            temp->next = head2;
+            head2 = temp;
+            temp = p1;
+        }
+        ListNode *head1 = head;
+        
+        //merge the two lists
+        while(head2)
+        {
+            p1 = head1->next;
+            head1->next = head2;
+            p2 = head2->next;
+            head2->next = p1;
+            head1 = p1;
+            head2 = p2;
+        }
+    }
+};
