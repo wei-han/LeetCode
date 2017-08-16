ff04d1af984390c8ccc826a97c6c3852ea3c6a1d
Han Wei <jensonwei2012@gmail.com>
2017-08-16 17:26:08 -0400

    Create 155_Min Stack.cpp

---
+++ 155_Min Stack.cpp
@@ -0,0 +1,37 @@
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        s1.push(x);
+        if(s2.empty() || x <= getMin()) s2.push(x);
+    }
+    
+    void pop() {
+        if(top() == getMin()) s2.pop(); //top before pop!!!
+        s1.pop();
+    }
+    
+    int top() {
+        return s1.top();
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
+private:
+    stack<int> s1;
+    stack<int> s2;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
