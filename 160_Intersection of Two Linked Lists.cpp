4c8f8109f35121e0f4df584005c68065b09b0633
Han Wei <jensonwei2012@gmail.com>
2017-08-17 13:49:50 -0400

    Update 160_Intersection of Two Linked Lists.cpp

--- 160_Intersection of Two Linked Lists.cpp
+++ 160_Intersection of Two Linked Lists.cpp
@@ -39,3 +39,12 @@ public:
         return A_2;
     }
 };
+//refactory version
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode *cur1 = headA, *cur2 = headB;
+    while(cur1 != cur2){
+        cur1 = cur1?cur1->next:headB;
+        cur2 = cur2?cur2->next:headA;
+    }
+    return cur1;
+}
