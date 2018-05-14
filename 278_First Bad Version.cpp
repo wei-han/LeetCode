674cff01786ba93c9b3a75b2a7d7687251d359b9
Han Wei <jensonwei2012@gmail.com>
2018-05-13 23:04:59 -0400

    Create 278_First Bad Version.cpp

---
+++ 278_First Bad Version.cpp
@@ -0,0 +1,18 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low=1;                                       //starts from 1
+        int high=n;                                     //ends at n
+        while(low<high)
+        {
+            int mid= low + (high-low)/2;
+            
+            if(isBadVersion(mid)) high=mid;
+            else low=mid+1;
+        }
+        return low;
+    }
+};
