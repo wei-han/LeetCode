06c6a3c80524dd1d13854b3b630a1ba47c388944
Han Wei <jensonwei2012@gmail.com>
2018-05-21 03:30:00 -0400

    Create 326_Power of Three.cpp

---
+++ 326_Power of Three.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n==0) return false;
+        while(n!=1){
+            if(n%3!=0) return false;
+            n = n/3;
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        return n>0 && 1162261467%n ==0;
+    }
+};
