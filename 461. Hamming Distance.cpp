e58c821843d5db2cd81f21e1cfdc4c8acf61d2f2
Han Wei <jensonwei2012@gmail.com>
2018-06-20 21:28:10 -0400

    Create 461. Hamming Distance.cpp

---
+++ 461. Hamming Distance.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int n = x^y;
+        int dist = 0;
+        
+        while(n){
+            dist++;
+            n &= n-1;
+        }
+        return dist;
+    }
+};
