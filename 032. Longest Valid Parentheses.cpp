068eb9d897b7d334ce054b3e4fa725a436daa82d
Han Wei <jensonwei2012@gmail.com>
2018-08-12 18:11:49 -0700

    Create 032. Longest Valid Parentheses.cpp

---
+++ 032. Longest Valid Parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> stk;
+        stk.push(-1);
+        int res = 0;
+        for(int i = 0; i<s.size(); i++){
+            int t = stk.top();
+            if(t>-1 && s[i]==')' && s[t]=='('){
+                stk.pop();
+                res = max(res, i-stk.top());
+            }
+            else{
+                stk.push(i);
+            }
+        }
+        return res;
+    }
+};
